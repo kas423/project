0b9e45d20e01f969d34019dea34a5dd3d30149f6
Krutarth Patel <krutarth07@gmail.com>
2016-08-25 22:17:12 +0530

    hi

--- app/src/main/java/com/java/arobjviewer/MainActivity.java
+++ app/src/main/java/com/java/arobjviewer/MainActivity.java
@@ -17,7 +17,7 @@ import android.widget.TextView;
 import edu.dhbw.andarmodelviewer.R;
 
 //hi there
-//dfskcnw
+//dfskcnwv
 public class MainActivity extends Activity {
 
 	public static final String PREFS_NAME = "MyPrefsFile";
